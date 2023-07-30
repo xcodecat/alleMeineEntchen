6bd07e20363ba5cd47aa130285e131de8d3db664
xcodecat <xmissh0lyc4t@gmail.com>
2023-07-30 17:21:36 +0200

    feat(alleMeineEntchen): fixed melody
    - changed duration
    - changed tempo
    - changed pause between tones
    - changed delay
    - fixed char names[]

--- alleMeineEntchen.ino
+++ alleMeineEntchen.ino
@@ -6,26 +6,24 @@ void setup() {
 
 void loop() {
   alleMeineEntchen();
-  delay(2000);
 }
 
 void alleMeineEntchen() {
-  char notes[] = "cdef gg aaaa g aaaa g ffff ee dddd c ";
+  char notes[] = "cdef gg aaaa g aaaa g ffff ee dddd c";
   int beats[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-  int tempo = 150;
+  int tempo = 300;
   const int songLength = 37;
 
   int i, duration;
   for (i = 0; i < songLength; i++) {
-    duration = (60000 / (beats[i] * tempo));
+    duration = beats[i] * tempo;
     if (notes[i] == ' ') {
-      noTone(buzzerPin);
+      delay(duration);
     } else {
       tone(buzzerPin, frequency(notes[i]), duration);
     }
-    delay(duration * 1.30);
-    noTone(buzzerPin);
-    delay(tempo / 5);
+    delay(duration);
+    delay(tempo / 10);
   }
 }
 
@@ -33,7 +31,7 @@ int frequency(char note) {
   int i;
   const int numNotes = 8;
 
-  char names[] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c'};
+  char names[] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C'};
   int frequency[] = {262, 294, 330, 349, 392, 440, 494, 523};
 
   for (i = 0; i < numNotes; i++) {
